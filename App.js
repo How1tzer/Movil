ea8671ac47e701d3ab15a2fcf3d9c24188c5aa5e
Eduardo Marges <135554783+How1tzer@users.noreply.github.com>
2023-09-22 18:08:23 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { NativeBaseProvider, Box, Container } from "native-base";
-import CustomInput from "./CustomInput";
-import Login from "./Components/Login";
+import Formulario from "./components/Formulario";
+import Tabla from "./components/Tabla";
 
 export default function App() {
   return (
     <NativeBaseProvider>
       <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
         <Container>
-          <Login />
-          {/* Este componente es solo un ejemplo */}
-          <CustomInput text="Usuario" />
+          <Formulario />
+          {/* Aquí puedes agregar el componente Tabla y pasarle los usuarios añadidos como parámetro */}
+          {/* Ejemplo: */}
+          {/* <Tabla usuarios={usuarios} handleBorrar={handleBorrar} /> */}
         </Container>
       </Box>
     </NativeBaseProvider>
